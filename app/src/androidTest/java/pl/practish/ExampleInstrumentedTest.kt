3252c398048ffe792e834893d794dca32c97b34c
Maciej Sikorski <maciej.sikorski@allegro.pl>
2019-03-23 22:05:46 +0100

    Android project files

---
+++ app/src/androidTest/java/pl/practish/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package pl.practish
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun useAppContext() {
+        val appContext = ApplicationProvider.getApplicationContext<MainActivity>()
+        assertEquals("pl.practish", appContext.packageName)
+    }
+
+}
